e02682d99073afc960b997b54538a66b48f31e11
Gibb-y <98590606+Gibb-y@users.noreply.github.com>
2025-06-08 23:59:53 +0200

    Agents in levels

--- Assets/Scripts/PlayingState.cs
+++ Assets/Scripts/PlayingState.cs
@@ -1,8 +1,35 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayingState : IState
 {
-    public void OnEnter() { Time.timeScale = 1f; Debug.Log("Game Started"); }
-    public void Update() { }
+    private HealthManager[] entities;
+    public void OnEnter() 
+    { 
+        Time.timeScale = 1f; Debug.Log("Game Started");
+        var roots = SceneManager.GetActiveScene().GetRootGameObjects();
+        foreach (var root in roots)
+        {
+            var ents = root.GetComponentsInChildren<HealthManager>();
+            if (ents is not null && ents.Length != 0)
+            {
+                entities = ents;
+            }
+        }
+        Debug.Log(roots);
+    }
+    public void Update() 
+    {
+        if (entities is null)
+            return;
+        foreach (var entt in entities)
+        {
+            if (entt.health <= 0)
+            {
+                GameManager.Instance.OnStopPressed();
+            }
+        }
+    }
     public void OnExit() { }
 }
