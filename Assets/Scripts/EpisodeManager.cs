d15f95581d46a200bfb4404efc77220b25bd86af
PT <113798772+pgm-stenhullebroeck@users.noreply.github.com>
2025-06-08 10:29:05 +0200

    Added: lot of changes to main and sniper agent script

---
+++ Assets/Scripts/EpisodeManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using UnityEngine;
+
+public class EpisodeManager : MonoBehaviour
+{
+	public List<Agent> agents;
+	private SimpleMultiAgentGroup agentGroup;
+
+	private void Start()
+	{
+		agents = new List<Agent>(gameObject.GetComponentsInChildren<Character>());
+
+		agentGroup = new SimpleMultiAgentGroup();
+
+		foreach (var agent in agents)
+		{
+			agentGroup.RegisterAgent(agent);
+		}
+
+		foreach (var agent in agents)
+			Debug.Log(agent);
+	}
+
+	public void EndAllEpisodes()
+	{
+		StartCoroutine(EndAllEpisodesNextFrame());
+	}
+
+	private IEnumerator EndAllEpisodesNextFrame()
+	{
+		yield return null; // wait one frame
+
+		foreach (var agent in agents)
+		{
+			Debug.Log($"Ending episode for {agent.name}");
+			agent.EndEpisode();
+		}
+	}
+}
